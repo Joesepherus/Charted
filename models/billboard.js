b3aa47bddb4f5c6ddd0e7683c87c19721f1d182e
Jozef Maloch <jozef.maloch@agentselly.ch>
2019-12-21 21:00:39 +0100

    Very frustrating so apparently mongoose takes plural of the collection name unless u defined it in the model as third argument...

--- models/billboard.js
+++ models/billboard.js
@@ -12,14 +12,10 @@ var schema = mongoose.Schema({
 });
 
 
-var Billboard = module.exports = mongoose.model('billboard', schema);
+var Billboard = module.exports = mongoose.model('billboard', schema, 'billboard');
 
 module.exports.getAllSongs = function (callback, limit) {
 	// Billboard.find(callback).limit(limit);
-	Billboard.find({}, function (err, docs) {
-		console.log('docs: ', docs);
-		// do momething
-	})
 
 	Billboard.find(callback).limit();
 }
