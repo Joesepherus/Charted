fdd424c49794c22aac5cf0dd32f891ba7d6dde07
Jozef Maloch <jozef.maloch@agentselly.ch>
2019-12-21 20:41:55 +0100

    test billboard collection find.

--- models/billboard.js
+++ models/billboard.js
@@ -20,6 +20,8 @@ var Billboard = module.exports = mongoose.model('billboard', schema);
 
 module.exports.getAllSongs = function (callback, limit) {
 	// Billboard.find(callback).limit(limit);
+	const songs = Billboard.find().fetch()
+	console.log('songs: ', songs);
 
 	Billboard.find(callback).limit();
 }
