d7837dd96ef6617ddd1658ebbae2ccfd7195bbe1
Jozef Maloch <jozef.maloch@agentselly.ch>
2019-12-21 20:48:12 +0100

    Schema update for billboard.

--- models/billboard.js
+++ models/billboard.js
@@ -1,10 +1,6 @@
 var mongoose = require('mongoose');
 
 var schema = mongoose.Schema({
-	id: {
-		type: Number,
-		required: true
-	},
 	title: {
 		type: String,
 		required: true
@@ -13,15 +9,13 @@ var schema = mongoose.Schema({
 		type: String,
 		required: true
 	},
-}, { _id: false });
+});
 
 
 var Billboard = module.exports = mongoose.model('billboard', schema);
 
 module.exports.getAllSongs = function (callback, limit) {
 	// Billboard.find(callback).limit(limit);
-	const songs = Billboard.find()
-	console.log('songs: ', songs);
 
 	Billboard.find(callback).limit();
 }
